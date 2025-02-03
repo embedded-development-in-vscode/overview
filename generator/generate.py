cdc3ea8ab2e917d9d21a24f979ce974bdc2cb5a1
Ivan Kravets <me@ikravets.com>
2025-02-03 12:16:55 +0200

    Fix vsmarketplace/issues/1114

--- generator/generate.py
+++ generator/generate.py
@@ -131,10 +131,9 @@ def generate_analytics_data(extensions, dst_dir):
     with httpx.Client() as http:
         data = http.get(ANALYTICS_DATA_URL).json()
 
-    diff = data[len(data) - 1]["extensions"][6]["icnt"] - data[len(data) - 2]["extensions"][6]["icnt"]
-    data[len(data) - 3]["extensions"][6]["icnt"] = data[len(data) - 4]["extensions"][6]["icnt"] + (diff * 1.05)
+    del [data[-1]]
 
-    if os.environ.get("GITHUB_EVENT_NAME") == "schedule" or True:
+    if os.environ.get("GITHUB_EVENT_NAME") == "schedule":
         data.append(
             {
                 "timestamp": time.time(),
