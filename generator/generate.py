047b8ba06381e14b051aaa8e78b8333d405686c9
Ivan Kravets <me@ikravets.com>
2025-02-03 11:18:25 +0200

    Re-schedule job

--- generator/generate.py
+++ generator/generate.py
@@ -131,7 +131,7 @@ def generate_analytics_data(extensions, dst_dir):
     with httpx.Client() as http:
         data = http.get(ANALYTICS_DATA_URL).json()
 
-    if os.environ.get("GITHUB_EVENT_NAME") == "schedule":
+    if os.environ.get("GITHUB_EVENT_NAME") == "schedule" or True:
         data.append(
             {
                 "timestamp": time.time(),
